b83e85d68e0070f91fa4b86fb25de07d0ec9905f
print-dev <alexisjkg@gmail.com>
2024-10-11 12:32:02 -0500

    interfaz de registro de cliente

--- views/clientes/listar-clientes.php
+++ views/clientes/listar-clientes.php
@@ -5,15 +5,59 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
-  <?php echo renderContentHeader("Clientes", "Inicio") ?>
+  <?php echo renderContentHeader("Clientes", "Inicio", SERVERURL . "views") ?>
   <!-- /.content-header -->
 
   <!-- Main content -->
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <!-- Contenido personalizado -->
-
+        <div class="col-md-12">
+          <a href="./registrar-cliente" class="btn btn-primary btn-sm">Registrar</a>
+          <hr>
+          <div class="table-responsive">
+            <table class="table table-sm">
+              <thead>
+                <th>#</th>
+                <th>Apellidos</th>
+                <th>Nombres</th>
+                <th>Telefono</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Avalos Romero</td>
+                  <td>Royer Alexis</td>
+                  <td>973189350</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Avalos Romero</td>
+                  <td>Royer Alexis</td>
+                  <td>973189350</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Avalos Romero</td>
+                  <td>Royer Alexis</td>
+                  <td>973189350</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Avalos Romero</td>
+                  <td>Royer Alexis</td>
+                  <td>973189350</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Avalos Romero</td>
+                  <td>Royer Alexis</td>
+                  <td>973189350</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
       <!-- /.row -->
     </div><!-- /.container-fluid -->
@@ -22,7 +66,7 @@
 </div>
 <!-- /.content-wrapper -->
 
-<?php require_once "./includes/footer.php"; ?>
+<?php require_once "../includes/footer.php"; ?>
 
 </body>
 
--- views/clientes/registrar-cliente.php
+++ views/clientes/registrar-cliente.php
@@ -1,1 +1,73 @@
-<h1>registrar cliente</h1>
+<?php require_once '../../app/config/App.php'; ?>
+
+<?php require_once "../includes/header.php"; ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <?php echo renderContentHeader("Nuevo Cliente", "Lista de clientes", SERVERURL . "views/clientes/listar-clientes") ?>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <!-- Contenido personalizado -->
+        <div class="col-md-12">
+          <!-- FILA 1 -->
+          <form action="" id="formulario-clientes" autocomplete="off">
+            <div class="card card-outline card-primary">
+              <div class="card-header">
+                Complete los datos
+              </div>
+              <div class="card-body">
+                <!-- FILA 1 -->
+                <div class="row">
+                  <div class="col-md-4 form-group">
+                    <label for="apellidos">Apellidos</label>
+                    <input type="text" class="form-control" id="apellidos" autofocus required>
+                  </div>
+                  <div class="col-md-4 form-group">
+                    <label for="apellidos">Nombres</label>
+                    <input type="text" class="form-control" id="nombres" required>
+                  </div>
+                  <div class="col-md-4 form-group">
+                    <label for="apellidos">Telefono</label>
+                    <input type="tel" class="form-control text-center" minlength="9" maxlength="9" id="telefono" required>
+                  </div>
+                </div>
+                <!-- FIN FILA 1 -->
+              </div>
+              <div class="card-footer text-right">
+                <button type="submit" class="btn btn-sm btn-primary">Guardar</button>
+              </div>
+            </div> <!-- ./card -->
+          </form>
+        </div> <!-- ./col-md-12 -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php require_once "../includes/footer.php"; ?>
+
+<script>
+  document.addEventListener("DOMContentLoaded", ()=>{
+    document.querySelector("#formulario-clientes").addEventListener("submit", async (e)=>{
+      e.preventDefault()
+      if(await ask("¿Desea guardar?", "Clientes")){
+        //Algoritmo para guardar
+        //GET = URLSearchParams()
+        //POST = FormData()
+        showToast("Guardado correctamente", "SUCCESS", 1000, "./listar-clientes")
+      }
+    })
+  })
+</script>
+
+</body>
+
+</html>
